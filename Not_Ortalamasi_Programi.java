5e9e3696b2d893f676b313e183be50a555a1b056
İlhan DARICI <85981579+ilhandarici@users.noreply.github.com>
2022-04-21 17:20:38 +0300

    Dosyalar düzenlendi.

---
+++ Not_Ortalamasi_Programi.java
@@ -0,0 +1,48 @@
+package NotOrtalamasiProgrami;
+import java.util.Scanner;
+public class Not_Ortalamasi_Programi {
+    public static void main(String[] args) {
+    
+     // eğer kullanıcının ortalaması 60'dan büyük ise ekrana "Sınıfı Geçti" , küçük ise "Sınıfta Kaldı" yazsın.
+    
+     // scanner atandı.
+     Scanner inp = new Scanner(System.in);
+     
+     // değişkenler tanımlandı.
+     int mat,fiz,kim,tur,tar,muzik,toplam;
+     
+     // kullanıcıdan veri alındı.
+     try {
+     System.out.print("Matematik dersi notunuzu giriniz :");
+     mat = inp.nextInt();
+
+     System.out.print("Fizik dersi notunuzu giriniz :");
+     fiz = inp.nextInt();
+
+     System.out.print("Kimya dersi notunuzu giriniz :");
+     kim = inp.nextInt();
+
+     System.out.print("Türkçe dersi notunuzu giriniz :");
+     tur = inp.nextInt();
+
+     System.out.print("Tarih dersi notunuzu giriniz :");
+     tar = inp.nextInt();
+
+     System.out.print("Müzik dersi notunuzu giriniz :");
+     muzik = inp.nextInt();
+     } finally{
+         inp.close();
+     }
+    
+     // matematiksel işlemler yapıldı. 
+     toplam = mat + fiz + kim + tur + tar + muzik;
+     double sonuc = toplam / 6;
+
+     // koşul işlemi yapıldı.
+     String kosul = (sonuc >= 60) ? "Sınıfı Geçti" : "Sınıfta Kaldı";
+     System.out.println("Not ortalamanız : "+sonuc);
+     System.out.println("Sınıfı Geçme/Kalma durumunuz : "+kosul);
+     
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,17 @@
+# Not Ortalaması Hesaplama Programı
+-------------------------------------------------------------------- 
+### Programın amacı
+
+**Bu program Kullanıcıdan alınan not verilerinin aritmetik ortamalamasını alacak, bu ortalmayı çıktı olarak verecek ve bu ortalamanın sonucunda 60 ve üzeri ise; "Sınıfı Geçti.", değilse; "Sınıfta Kaldı." çıktısını verecektir.** 
+
+### Programdan Görüntüler
+![ss1](https://user-images.githubusercontent.com/85981579/164350382-152d81a0-4312-4f5b-b65b-b36a8f83b828.PNG)
+
+![ss2](https://user-images.githubusercontent.com/85981579/164350459-eb969e55-af7d-4054-a46b-cd9f64362095.PNG)
+--------------------------------------------------------------------
+
+
+
+***Bu proje Kodluyoruz Back-end Bootcamp Hazırlık Süreci dolayısıyla ödev amacıyla oluşturulmuştur.***
+[Patika Dev](https://www.patika.dev)
+
